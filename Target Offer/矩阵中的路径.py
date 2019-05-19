6523c4ea105af24032180f06b23dd9759e108683
Einstellung <kekedog@126.com>
2019-05-20 03:18:45 +0800

    添加第12题注释

--- Target Offer/矩阵中的路径.py
+++ Target Offer/矩阵中的路径.py
@@ -9,38 +9,41 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def hasPath(self, matrix, rows, cols, path):
-        if matrix == None or rows < 1 or cols < 1 or path == None:
+        if len(matrix) == 0 or rows < 1 or cols < 1 or len(path) == 0:
             return False
-        visited = [0] * (rows * cols)
-
+        
         pathLength = 0
+        visited = [0] * (rows * cols)
+        
         for row in range(rows):
             for col in range(cols):
                 if self.hasPathCore(matrix, rows, cols, row, col, path, pathLength, visited):
                     return True
+                
         return False
-
+    
     def hasPathCore(self, matrix, rows, cols, row, col, path, pathLength, visited):
         if len(path) == pathLength:
             return True
-
-        hasPath = False
-        if row >= 0 and row < rows and col >= 0 and col < cols and matrix[row * cols + col] == path[pathLength] and not \
-        visited[row * cols + col]:
-
-            pathLength += 1
-            visited[row * cols + col] = True
-
-            hasPath = self.hasPathCore(matrix, rows, cols, row, col - 1, path, pathLength, visited) or \
-                      self.hasPathCore(matrix, rows, cols, row - 1, col, path, pathLength, visited) or \
-                      self.hasPathCore(matrix, rows, cols, row, col + 1, path, pathLength, visited) or \
-                      self.hasPathCore(matrix, rows, cols, row + 1, col, path, pathLength, visited)
-
+        
+        hasPath = False      # hasPath表示最后是否存在匹配路径
+        
+        if row >= 0 and col >= 0 and row < rows and col < cols and matrix[row*cols + col] == path[pathLength] and not \
+        visited[row*cols + col]:
+            
+            pathLength += 1  # 这个元素比较完了，之后比较下一个元素
+            visited[row*cols + col] = True  # visited表示已经访问过的位置，访问过就变成True
+            
+            hasPath = self.hasPathCore(matrix, rows, cols, row+1, col, path, pathLength, visited) or \
+                      self.hasPathCore(matrix, rows, cols, row-1, col, path, pathLength, visited) or \
+                      self.hasPathCore(matrix, rows, cols, row, col+1, path, pathLength, visited) or \
+                      self.hasPathCore(matrix, rows, cols, row, col-1, path, pathLength, visited)
+            
             if not hasPath:
                 pathLength -= 1
-                visited[row * cols + col] = False
-
-        return hasPath
+                visited[row*cols + col] = False
+                
+        return hasPath  
 
 s = Solution()
 ifTrue = s.hasPath("ABCESFCSADEE", 3, 4, "ABCCED")
