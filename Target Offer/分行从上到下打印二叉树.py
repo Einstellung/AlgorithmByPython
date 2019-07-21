c7f3c679547a2161189d78869160a0543a2958ac
Einstellung <kekedog@126.com>
2019-07-21 17:16:20 +0800

    更改缩进

--- Target Offer/分行从上到下打印二叉树.py
+++ Target Offer/分行从上到下打印二叉树.py
@@ -13,41 +13,41 @@
 一个变量表示当前层中还没有打印的节点数；另一个变量表示下一层节点数
 '''
 class TreeNode:
-  def __init__(self, x):
-    self.val = x
-    self.left = None
-    self.right = None
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 class Solution:
-  def Print(self, root):
-    if not root:
-      return None
+      def Print(self, root):
+        if not root:
+            return None
     
-    queue = [root]
-    toBePrinted = 1  # 表示当前层中还没有打印的节点数
-    nextLevel = 0    # 表示下一层的节点数
+        queue = [root]
+        toBePrinted = 1  # 表示当前层中还没有打印的节点数
+        nextLevel = 0    # 表示下一层的节点数
     
-    while len(queue) > 0:
-      currentRoot = queue.pop(0)
-      # 按空格隔开，不换行输出
-      print(currentRoot.val, end=" ")
-      
-      if currentRoot.left:
-        queue.append(currentRoot.left)
-        nextLevel += 1
-      if currentRoot.right:
-        queue.append(currentRoot.right)
-        nextLevel += 1
-          
-      toBePrinted -= 1
-  
-      # 如果当前层未打印的节点数为0，就跳转到下一层
-      if toBePrinted == 0:
-        # 如果下一层没有东西了，就不再执行程序了
-        if nextLevel == 0:
-          break
-        print("\n")
-        toBePrinted = nextLevel
-        nextLevel = 0
+        while len(queue) > 0:
+            currentRoot = queue.pop(0)
+          # 按空格隔开，不换行输出
+            print(currentRoot.val, end=" ")
+
+            if currentRoot.left:
+                queue.append(currentRoot.left)
+                nextLevel += 1
+            if currentRoot.right:
+                queue.append(currentRoot.right)
+                nextLevel += 1
+
+            toBePrinted -= 1
+
+            # 如果当前层未打印的节点数为0，就跳转到下一层
+            if toBePrinted == 0:
+            # 如果下一层没有东西了，就不再执行程序了
+                if nextLevel == 0:
+                    break
+                print("\n")
+                toBePrinted = nextLevel
+                nextLevel = 0
 
 pNode1 = TreeNode(8)
 pNode2 = TreeNode(6)
