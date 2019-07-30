78543bd308ded10c851f5c5be13060ed7bb10825
Einstellung <kekedog@126.com>
2019-07-30 22:39:25 +0800

    更改38题代码

--- Target Offer/字符串的排列和组合.py
+++ Target Offer/字符串的排列和组合.py
@@ -9,21 +9,17 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def Permutation(self, ss):
-        if not len(ss):
+        # write code here
+        if len(ss) == 0:
             return []
         if len(ss) == 1:
-            return list(ss)
-
-        charList = list(ss)
-        charList.sort()
-        pStr = []
-        for i in range(len(charList)):
-            if i > 0 and charList[i] == charList[i-1]:
-                continue
-            temp = self.Permutation(''.join(charList[:i])+''.join(charList[i+1:]))
-            for j in temp:
-                pStr.append(charList[i]+j)
-        return pStr
+            return [ss]
+        res = set()
+         #遍历字符串，固定第一个元素，第一个元素可以取a,b,c...全部取到，然后递归求解
+        for i in range(len(ss)):
+            for j in self.Permutation(ss[:i] + ss[i+1:]):#依次固定了元素，然后递归求解
+                res.add(ss[i]+j)  #set集合添加元素的方法add(),集合添加去重（若存在重复字符，排列后会存在相同情况）
+        return sorted(res)
 
     # 扩展习题, 生成字符的所有组合
     # 比如输入abc, 则他们的组合有['a', 'ab', 'abc', 'ac', 'b', 'bc', 'c'], ab和ba属于不同的排列, 但属于同一个组合
