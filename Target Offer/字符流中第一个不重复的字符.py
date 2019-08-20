d60d58a5e232553a13de50ca054ecd347bbcce41
Einstellung <kekedog@126.com>
2019-08-20 11:45:53 +0800

    更改50题题目2代码

--- Target Offer/字符流中第一个不重复的字符.py
+++ Target Offer/字符流中第一个不重复的字符.py
@@ -8,18 +8,30 @@
 # -*- coding:utf-8 -*-
 class Solution:
     def __init__(self):
-        self.adict = {}
         self.alist = []
+        self.adict = {}
+        
     def FirstAppearingOnce(self):
-        while len(self.alist) > 0 and self.adict[self.alist[0]] == 2:
+        # 当列表中的首字符重复出现，就将其剔除列表
+        while len(self.alist) > 0 and self.adict[self.alist[0]] > 1:
             self.alist.pop(0)
         if len(self.alist) == 0:
-            return '#'
+            return "all chars are repeated"
         else:
             return self.alist[0]
+            
+    
     def Insert(self, char):
-        if char not in self.adict.keys():
-            self.adict[char] = 1
-            self.alist.append(char)
-        elif self.adict[char]:
-            self.adict[char] = 2
+        # 注意输入的是字符流，而不一定是字母
+        # 所以不能用isalpha来判断
+#         if not char.isalpha():
+#             return "please input char"
+        if not char:
+            return "please input char"
+        # 同上一个题一样，需要一个字符一个字符来导入字典进行判断
+        for i in char:
+            self.alist.append(i)
+            if i not in self.adict.keys():
+                self.adict[i] = 1
+            else:
+                self.adict[i] += 1
