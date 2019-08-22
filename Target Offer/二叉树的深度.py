e87e9fbea35aa18f067c5ae2f1b64e9b73a12b17
Einstellung <kekedog@126.com>
2019-08-22 15:47:21 +0800

    更改55题代码

--- Target Offer/二叉树的深度.py
+++ Target Offer/二叉树的深度.py
@@ -9,33 +9,30 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+
 class Solution:
-    # 递归解法, 简单直接, 时间复杂度O(n), 空间复杂度O(logn)
     def TreeDepth(self, pRoot):
         if pRoot == None:
             return 0
-        else:
-            return max(self.TreeDepth(pRoot.left), self.TreeDepth(pRoot.right)) + 1
-    # 非递归算法，利用一个栈以及一个标志位栈
-    def TreeDepth2(self, pRoot):
-        if not pRoot:
-            return 0
-        depth = 0
-        stack, tag = [], []
-        pNode = pRoot
-        while pNode or stack:
-            while pNode:
-                stack.append(pNode)
-                tag.append(0)
-                pNode = pNode.left
-            if tag[-1] == 1:
-                depth = max(depth, len(stack))
-                stack.pop()
-                tag.pop()
-                pNode = None
-            else:
-                pNode = stack[-1]
-                pNode = pNode.right
-                tag.pop()
-                tag.append(1)
-        return depth
+        
+        left = self.TreeDepth(pRoot.left)
+        right = self.TreeDepth(pRoot.right)
+        
+        return max(left, right) + 1
+
+pNode1 = TreeNode(1)
+pNode2 = TreeNode(2)
+pNode3 = TreeNode(3)
+pNode4 = TreeNode(4)
+pNode5 = TreeNode(5)
+pNode6 = TreeNode(7)
+
+
+pNode1.left = pNode2
+pNode1.right = pNode3
+pNode2.left = pNode4
+pNode2.right = pNode5
+pNode5.left = pNode6
+
+a = Solution()
+a.TreeDepth(pNode1)
