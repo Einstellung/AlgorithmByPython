672ac8f6561fc56813c6b510c75460576ee3692d
Einstellung <kekedog@126.com>
2019-08-22 17:05:37 +0800

    更改平衡二叉树代码

--- Target Offer/判断平衡二叉树.py
+++ Target Offer/判断平衡二叉树.py
@@ -8,51 +8,47 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+        
 class Solution:
     def __init__(self):
         self.flag = True
-
+        
     def IsBalanced_Solution(self, pRoot):
         self.getDepth(pRoot)
         return self.flag
-
+    
     def getDepth(self, pRoot):
         if pRoot == None:
             return 0
-        left = 1 + self.getDepth(pRoot.left)
-        right = 1 + self.getDepth(pRoot.right)
-
+        
+        # 这是后续遍历的特点
+        left = self.getDepth(pRoot.left) + 1
+        right = self.getDepth(pRoot.right) + 1
+        
+        # 判断该子树的左右节点长度是否超过1，即判断
+        # 该子树是否为平衡二叉树，每个子树都是平衡二叉树
+        # 那么总体也是平衡二叉树
         if abs(left - right) > 1:
             self.flag = False
-
-        return left if left > right else right
-class Solution2:
-    def getDepth(self, pRoot):
-        if pRoot == None:
-            return 0
-        return max(self.getDepth(pRoot.left), self.getDepth(pRoot.right)) + 1
-    def IsBalanced_Solution(self, pRoot):
-        if pRoot == None:
-            return True
-        if abs(self.getDepth(pRoot.left)-self.getDepth(pRoot.right)) > 1:
-            return False
-        return self.IsBalanced_Solution(pRoot.left) and self.IsBalanced_Solution(pRoot.right)
-
+        
+        # 如果一个节点有多个分支，要取最长分支
+        # 只有最长的分支才是二叉树的深度
+        return max(left, right) 
 
 pNode1 = TreeNode(1)
 pNode2 = TreeNode(2)
 pNode3 = TreeNode(3)
 pNode4 = TreeNode(4)
 pNode5 = TreeNode(5)
-pNode6 = TreeNode(6)
-pNode7 = TreeNode(7)
+pNode6 = TreeNode(7)
+pNode7 = TreeNode(6)
 
 pNode1.left = pNode2
 pNode1.right = pNode3
 pNode2.left = pNode4
 pNode2.right = pNode5
-pNode3.right = pNode6
-pNode5.left = pNode7
+pNode5.left = pNode6
+pNode3.right = pNode7
 
-S = Solution2()
-print(S.getDepth(pNode1))
+a = Solution()
+a.IsBalanced_Solution(pNode1)
