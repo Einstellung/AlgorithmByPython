5a8ae5ead1513aa687c361101a46abc47e7dbfef
Einstellung <kekedog@126.com>
2019-08-23 11:30:26 +0800

    更改56题题目一代码

--- Target Offer/数组中只出现一次的数字.py
+++ Target Offer/数组中只出现一次的数字.py
@@ -9,12 +9,17 @@ class Solution:
         if array == None or len(array) <= 0:
             return []
         resultExclusiveOr = 0
+        # 从头到尾依次异或数组中的每个数字，最后得到的结果
+        # 就是两个只出现一次的数字异或的结果
         for i in array:
             resultExclusiveOr ^= i
-
+        
+        # 找到第一个为1的位置，之后按照是否为1将数组分成两部分
         indexOf1 = self.FindFirstBitIs1(resultExclusiveOr)
-        num1, num2 = 0
+
+        num1, num2 = 0, 0
         for j in range(len(array)):
+            # 如果array[j]移动indexOf1之后为1，那么就将其归到num1中，否则是num2中
             if self.IsBit1(array[j], indexOf1):
                 num1 ^= array[j]
             else:
@@ -23,43 +28,18 @@ class Solution:
 
     def FindFirstBitIs1(self, num):
         indexBit = 0
+        # indexBit用来检验是否超出字符取值范围
         while num & 1 == 0 and indexBit <= 32:
             indexBit += 1
             num = num >> 1
+        # 返回确定第几位字符为1
         return indexBit
 
     def IsBit1(self, num, indexBit):
+        # 没有办法直接比较最后一位，右移之后才好比较
         num = num >> indexBit
         return num & 1
 
-class Solution2:
-    # 返回[a,b] 其中ab是出现一次的两个数字
-    def FindNumsAppearOnce(self, array):
-        if array == None or len(array) <= 0:
-            return []
-        resultExOr = self.ExOr(array)
-        i = 0
-        while resultExOr and i <= 32:
-            i += 1
-            resultExOr = resultExOr>>1
-        num1, num2 = [], []
-        for num in array:
-            if self.bitIs1(num, i):
-                num1.append(num)
-            else:
-                num2.append(num)
-        first = self.ExOr(num1)
-        second = self.ExOr(num2)
-        return [first, second]
-
-    def ExOr(self, aList):
-        ExOrNum = 0
-        for i in aList:
-            ExOrNum = ExOrNum ^ i
-        return ExOrNum
-    def bitIs1(self, n, i):
-        n = n >> (i-1)
-        return n & 1
 
 aList = [2, 4, 3, 6, 3, 2, 5, 5]
 s = Solution()
